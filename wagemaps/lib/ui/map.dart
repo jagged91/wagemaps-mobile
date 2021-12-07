6df7fed21979130849f62ffebf43c82d9e385097
jagged91 <>
2021-12-06 22:24:25 -0500

    Custom info window

--- wagemaps/lib/ui/map.dart
+++ wagemaps/lib/ui/map.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:custom_info_window/custom_info_window.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -12,44 +13,112 @@ class MapScreen extends StatefulWidget {
 
 class _MapScreenState extends State<MapScreen> {
   Completer<GoogleMapController> _controller = Completer();
-
+  CustomInfoWindowController infoWindowController =
+      CustomInfoWindowController();
   static const CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(37.42796133580664, -122.085749655962),
     zoom: 14.4746,
   );
 
-  List<Marker> markers = [
-    const Marker(
-        flat: true,
-        icon: BitmapDescriptor.defaultMarker,
-        infoWindow: InfoWindow(title: "Google Pizza", snippet: "\$16 / hr"),
-        markerId: MarkerId("1"),
-        position: LatLng(37.42796133580664, -122.085749655962))
-  ];
+  List<Marker> markers = [];
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
   }
 
   @override
+  void dispose() {
+    infoWindowController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    markers = [
+      Marker(
+          onTap: () {
+            infoWindowController.addInfoWindow!(
+              Column(
+                children: [
+                  Expanded(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(4),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.monetization_on,
+                              color: Colors.white,
+                              size: 30,
+                            ),
+                            SizedBox(
+                              width: 8.0,
+                            ),
+                            Expanded(
+                              child: Text(
+                                "\$16 per hour",
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .headline6!
+                                    .copyWith(
+                                      color: Colors.white,
+                                    ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                      width: double.infinity,
+                      height: double.infinity,
+                    ),
+                  ),
+                ],
+              ),
+              LatLng(37.42796133580664, -122.085749655962),
+            );
+          },
+          markerId: MarkerId("1"),
+          position: LatLng(37.42796133580664, -122.085749655962))
+    ];
+
     return Scaffold(
       appBar: AppBar(
         systemOverlayStyle: SystemUiOverlayStyle.light,
         title: Text("Wagemaps"),
       ),
-      body: GoogleMap(
-        mapType: MapType.normal,
-        zoomControlsEnabled: false,
-        myLocationButtonEnabled: false,
-        mapToolbarEnabled: false,
-        markers: markers.toSet(),
-        initialCameraPosition: _kGooglePlex,
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
-        },
+      body: Stack(
+        children: [
+          GoogleMap(
+            mapType: MapType.normal,
+            zoomControlsEnabled: false,
+            myLocationButtonEnabled: false,
+            mapToolbarEnabled: false,
+            markers: markers.toSet(),
+            initialCameraPosition: _kGooglePlex,
+            onTap: (position) {
+              infoWindowController.hideInfoWindow!();
+            },
+            onCameraMove: (position) {
+              infoWindowController.onCameraMove!();
+            },
+            onMapCreated: (GoogleMapController controller) {
+              _controller.complete(controller);
+              infoWindowController.googleMapController = controller;
+            },
+          ),
+          CustomInfoWindow(
+            controller: infoWindowController,
+            height: 75,
+            width: 150,
+            offset: 50,
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
