7dc224c37a963aa4a79e2930761a725cd0c2e3dd
jagged91 <>
2021-12-06 21:55:57 -0500

    WIP adding google maps

--- wagemaps/lib/ui/map.dart
+++ wagemaps/lib/ui/map.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapScreen extends StatefulWidget {
   const MapScreen({Key? key}) : super(key: key);
@@ -8,6 +11,13 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
+  Completer<GoogleMapController> _controller = Completer();
+
+  static const CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,7 +25,13 @@ class _MapScreenState extends State<MapScreen> {
         systemOverlayStyle: SystemUiOverlayStyle.light,
         title: Text("Wagemaps"),
       ),
-      body: Text("hello"),
+      body: GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: _kGooglePlex,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         tooltip: 'Add wage',
